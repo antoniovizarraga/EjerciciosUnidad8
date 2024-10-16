9874b5bf740a59ff283e1ea0367374a7523dd521
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2024-10-16 14:31:15 +0200

    Ejercicio 1 terminado. Creación de ejercicio 2

---
+++ EjercicioLayout2/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace EjercicioLayout2
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
---
+++ EjercicioLayout2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EjercicioLayout2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
