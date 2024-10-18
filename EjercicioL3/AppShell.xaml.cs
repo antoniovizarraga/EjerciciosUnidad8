35553d60d29087a3f5a4b35485c4779229436a58
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2024-10-18 14:25:45 +0200

    Ejercicio 2 y 3 hecho (Me quiero morir xd. Por lo visto el 3 está mal porque hay que usar medidas relativas. Es decir, números de entre 0 y 1)

---
+++ EjercicioL3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EjercicioL3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EjercicioL3/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace EjercicioL3
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
